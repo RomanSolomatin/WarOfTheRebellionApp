82b3eb5799273b90c37bd8a3127d263a9c191f43
AustinMack9 <AustinMack@Austins-MBP.attlocal.net>
2017-03-06 10:41:27 -0600

    Changes to build

--- Build/Android/src/com/YourCompany/WarOfTheRebellionApp/OBBData.java
+++ Build/Android/src/com/YourCompany/WarOfTheRebellionApp/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-310532092L // the length of the file in bytes
+352668220L // the length of the file in bytes
 )
 };
 };
