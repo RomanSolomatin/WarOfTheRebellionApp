8d893f9fdf9b151cffe0c8fdd80d3d263a208262
AustinMack9 <AustinMack@Austins-MBP.attlocal.net>
2017-03-05 22:48:08 -0600

    Committing Most Recent Information

--- Build/Android/src/com/YourCompany/WarOfTheRebellionApp/OBBData.java
+++ Build/Android/src/com/YourCompany/WarOfTheRebellionApp/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-340268011L // the length of the file in bytes
+310532092L // the length of the file in bytes
 )
 };
 };
